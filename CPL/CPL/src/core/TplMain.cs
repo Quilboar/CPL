5e042052cb1f0b53f87488ef6aab6b1cb0d9801b
StefanBokarev <i_s.d.bokarev@mpt.com>
2020-08-10 15:16:23 +0300

    Rebuild to win forms

--- CPL/CPL/src/core/TplMain.cs
+++ CPL/CPL/src/core/TplMain.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CPL.src.core
 {
     class TplMain
     {
+        private string buf = "";
+        private string cr = "";
+        private enum States { S, SYMB, NUM, DEL, LST };
+        private States state;
+        private string[] Words = { "program", "var", "int", "real", "bool", "begin", "end", "if", "then", "else", "while", "do", "read", "write", "true", "false" };
+        private char[] Delimiter = { '.', ';', ',', ':', '=', '(', ')', '+', '-', '*', '/', '=', '>', '<' };
     }
 }
