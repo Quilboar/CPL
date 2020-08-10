48a38e7577dfff7371429d4e5922dc2c6fcfdc50
StefanBokarev <i_s.d.bokarev@mpt.com>
2020-08-10 18:24:58 +0300

    Add base methods

--- CPL/CPL/src/core/TplMain.cs
+++ CPL/CPL/src/core/TplMain.cs
@@ -9,10 +9,62 @@ namespace CPL.src.core
     class TplMain
     {
         private string buf = "";
-        private string cr = "";
+        private char ch;
         private enum States { S, SYMB, NUM, DEL, LST };
         private States state;
         private string[] Words = { "program", "var", "int", "real", "bool", "begin", "end", "if", "then", "else", "while", "do", "read", "write", "true", "false" };
         private char[] Delimiter = { '.', ';', ',', ':', '=', '(', ')', '+', '-', '*', '/', '=', '>', '<' };
+        
+
+        private void GetNext(char symb)
+        {
+            ch = symb;
+        }
+
+        private void ClearBuf()
+        {
+            buf = "";
+        }
+
+        private void AddBuf(char symb)
+        {
+            buf += symb;
+        }
+
+        private int SerchLex(string[] lexes)
+        {
+            var srh = lexes.AsEnumerable().Where(x => x == buf).ToString();
+            if (srh != "")
+                return Array.IndexOf(lexes, srh);
+            else return 0;
+        }
+
+        private int PushLex(string[] lexes)
+        {
+            var srh = lexes.AsEnumerable().Where(x => x == buf).ToString();
+            if (srh != "")
+                return 0;
+            else
+            {
+                Array.Resize(ref lexes, lexes.Length + 1);
+                lexes[lexes.Length - 1] = srh;
+                return lexes.Length - 1;
+            }
+        }
+
+        private void AddLex(string[] lexes, string val)
+        {
+            Array.Resize(ref lexes, lexes.Length + 1);
+            lexes[lexes.Length - 1] = val;
+        }
+
+        public void Analysis(string text)
+        {
+            foreach(char sm in text)
+            {
+                
+            }
+
+        }
     }
 }
